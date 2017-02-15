07f45d45d1c07df142ce0f111a4d110640d6ad27
竹本　伸吾 <takemotoshingo@takemotoshingo-no-MacBook-Air.local>
2017-02-15 15:38:53 +0900

    complete_login_function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action:move_to_index,except: :index
   def index
     @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -15,3 +16,8 @@ private
 def tweet_params
   params.permit(:name,:image,:text)
 end
+
+def move_to_index
+  redirect_to action: :index unless user_signed_in?
+end
+
