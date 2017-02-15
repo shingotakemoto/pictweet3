e39979165c2a95252601e734344201a1a511cb50
竹本　伸吾 <takemotoshingo@takemotoshingo-no-MacBook-Air.local>
2017-02-15 19:41:51 +0900

    complete my_page_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
-    @tweets = Tweet.all.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+    # @tweets = Tweet.all.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+    @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end
