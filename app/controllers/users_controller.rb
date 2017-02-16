d51be7cad85c3bc98c51ff404c70cd71faa6583a
竹本　伸吾 <takemotoshingo@takemotoshingo-no-MacBook-Air.local>
2017-02-16 13:11:31 +0900

    complete pic tweet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    # @tweets = Tweet.all.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
-    @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
-  end
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
+end
 end
